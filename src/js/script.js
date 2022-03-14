904402657ca39b70ac6a18672dab9d142d191cf8
KirillGalacti <89073958+KirillGalacti@users.noreply.github.com>
2022-03-14 20:05:28 +0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,21 @@
+$(document).ready(function () {
+  $(".second_div").hide();
+       $(".btn_1").click(function () {
+           $(".first_div").show();
+           $(".second_div").hide()
+           $(".third_div").hide()
+       });
+     $(".btn_2").click(function () {
+           $(".second_div").show();
+           $(".first_div").hide()
+           $(".third_div").hide()
+   });
+    $(".btn_3").click(function () {
+      $(".third_div").show()
+      $(".second_div").hide();
+      $(".first_div").hide()
+});
+});
 
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
@@ -10,3 +28,4 @@ jQuery(document).ready(function($) {
     //$("input[name='shippingfield[674b7be2-4b4f-4ad7-85cb-efdc8e15d2ab][value]']").mask("+7(999) 999-9999",{placeholder:"_"});
     $("input[name='form[tell]']").mask("+7 (999) 999-99-99");
 });	
+
