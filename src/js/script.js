4171b0056f60c5c77ee0bd0a34f7af8992844352
KirillGalacti <89073958+KirillGalacti@users.noreply.github.com>
2022-03-15 13:05:46 +0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+  
   $(".second_div").hide();
+  $(".third_div").hide();
        $(".btn_1").click(function () {
            $(".first_div").show();
            $(".second_div").hide()
@@ -16,6 +18,48 @@ $(document).ready(function () {
       $(".first_div").hide()
 });
 });
+
+const hover_base = document.querySelectorAll('.hover_base');
+const imgParent_base = document.querySelectorAll('.img-parent_base');
+
+hover_base.forEach((s, i) => {
+  s.addEventListener('mouseenter', () => {
+    hover_base.forEach((s, i) => {
+      imgParent_base[i].classList.remove('z');
+    })
+    imgParent_base[i].classList.add('z');
+
+  });
+});
+
+const hover_standart = document.querySelectorAll('.hover_standart');
+const imgParent_standart = document.querySelectorAll('.img-parent_standart');
+
+hover_standart.forEach((s, i) => {
+  s.addEventListener('mouseenter', () => {
+    hover_standart.forEach((s, i) => {
+      imgParent_standart[i].classList.remove('z');
+    })
+    imgParent_standart[i].classList.add('z');
+
+  });
+});
+
+const hover_expert = document.querySelectorAll('.hover_expert');
+const imgParent_expert = document.querySelectorAll('.img-parent_expert');
+
+hover_expert.forEach((s, i) => {
+  s.addEventListener('mouseenter', () => {
+    hover_expert.forEach((s, i) => {
+      imgParent_expert[i].classList.remove('z');
+    })
+    imgParent_expert[i].classList.add('z');
+
+  });
+});
+
+
+
 
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
